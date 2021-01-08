6657bf25ad003ca9da43e07c4b2cb8a9bd3940b9
champbvt <borvorntat.nir@gmail.com>
2021-01-08 12:46:11 +0700

    build

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_cors import CORS, cross_origin
+import os
 app = Flask(__name__)
 CORS(app)
 
@@ -9,4 +10,5 @@ def hello_world():
     return 'HEELO AD'
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port)
