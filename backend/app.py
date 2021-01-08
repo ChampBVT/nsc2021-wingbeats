15c85f42dce72c221245d443e7602df8c6835b63
champbvt <borvorntat.nir@gmail.com>
2021-01-08 13:43:50 +0700

    build

--- backend/app.py
+++ backend/app.py
@@ -10,5 +10,5 @@ def hello_world():
     return 'HEELO AD'
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 8000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
