72442b7bf09a2bd139710649d7e4dde66916d85e
champbvt <borvorntat.nir@gmail.com>
2021-01-26 17:33:04 +0700

    map prediction with string

--- backend/src/config.py
+++ backend/src/config.py
@@ -4,3 +4,5 @@ SAMPLING_RATE = 8000
 LENGTH_TARGET = 2400
 MODEL = 'model_1.h5'
 TIME_ZONE = 'Asia/Bangkok'
+GENDER_DICT = {0: 'Female', 1: 'Male'}
+SPECIES_DICT = {0: 'Aedes Aegypti', 1: 'Aedes Albopictus', 2: 'Anopheles Dirus', 3:'Anopheles Minimus', 4: 'Culex Quincuefaciatus' }
