1cb568335c92ebf0ab1861d24844f209c74fd67c
champbvt <borvorntat.nir@gmail.com>
2021-02-23 00:46:57 +0700

    change wording

--- frontend/src/components/ModalMoreDetail.vue
+++ frontend/src/components/ModalMoreDetail.vue
@@ -118,7 +118,7 @@ export default {
           width: '30%',
         },
         {
-          label: 'Probability',
+          label: 'Confidence',
           field: 'prob',
           type: 'number',
           thClass: 'text-center',
